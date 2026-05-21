3f94b99efd1a2543b68a9a937916334b4f323682
SpacePhil <spacephil@proton.me>
2026-05-21 18:45:07 +0200

    Implementing app version via setuptools / git tags

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from app import overlay
+from setuptools_scm import get_version
+
+APP_VERSION = get_version()
 
 def main():
     print("Application starting...")
+    print("Application version:", APP_VERSION)
 
     overlay.run()
 
